993ffc0531f545590ebca838e01bfa6c2774066a
ridwaanhall <erbyl14@gmail.com>
2024-12-10 15:51:10 +0700

    Add new routes for hero skill combo, hero rate, and hero relation; fix typo in hero detail stats path

--- mlbb_api/urls.py
+++ mlbb_api/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('hero-rank/', views.hero_rank, name='hero_rank'),
     path('hero-position/', views.hero_position, name='hero_position'),
     path('hero-detail/<int:hero_id>/', views.hero_detail, name='hero_detail'),
-    path('hero-detail-stats/<int:main_hero_id>/', views.hero_detail_stats, name='hero_detail_stats'),
-    path('hero-detail-combos/<int:hero_id>/', views.hero_detail_combos, name='hero_detail_combos'),
+    path('hero-detail-stats/<int:main_heroid>/', views.hero_detail_stats, name='hero_detail_stats'),
+    path('hero-skill-combo/<int:hero_id>/', views.hero_skill_combo, name='hero_skill_combo'),
+    path('hero-rate/<int:main_heroid>/', views.hero_rate, name='hero_rate'),
+    path('hero-relation/<int:hero_id>/', views.hero_relation, name='hero_relation'),
 ]

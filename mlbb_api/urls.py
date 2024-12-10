c29900f69981e02a5436f83530e132ac7cb56532
ridwaanhall <erbyl14@gmail.com>
2024-12-10 15:15:44 +0700

    Refactor URL paths for hero rank and position, and add new routes for hero detail, stats, and combos

--- mlbb_api/urls.py
+++ mlbb_api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hero-rank/', views.hero_rank, name='hero-rank'),
-    path('hero-position/', views.hero_position, name='hero-position'),
+    path('hero-rank/', views.hero_rank, name='hero_rank'),
+    path('hero-position/', views.hero_position, name='hero_position'),
+    path('hero-detail/<int:hero_id>/', views.hero_detail, name='hero_detail'),
+    path('hero-detail-stats/<int:main_hero_id>/', views.hero_detail_stats, name='hero_detail_stats'),
+    path('hero-detail-combos/<int:hero_id>/', views.hero_detail_combos, name='hero_detail_combos'),
 ]

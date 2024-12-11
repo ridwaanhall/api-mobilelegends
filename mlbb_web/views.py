15920708839089b75bdd60b552150305ac274334
ridwaanhall <erbyl14@gmail.com>
2024-12-11 19:56:24 +0700

    Add simple_view to return JSON response with status message

--- mlbb_web/views.py
+++ mlbb_web/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def simple_view(request):
+    data = {
+        "code": 200,
+        "status": "ok",
+        "message": "Hello ridwaanhall"
+    }
+    return JsonResponse(data)

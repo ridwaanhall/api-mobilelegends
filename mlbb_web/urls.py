baa7674e8fcf7510c6942ec3955bc4a7b62985bf
ridwaanhall <erbyl14@gmail.com>
2024-12-11 21:31:38 +0700

    Add URL patterns for hero rank and position views

--- mlbb_web/urls.py
+++ mlbb_web/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.simple_view, name='simple_view'),
+    path('hero-rank/', views.hero_rank_web, name='hero_rank_web'),
+    path('hero-position/', views.hero_position_web, name='hero_position_web'),
 ]

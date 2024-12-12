8d082fe5a75fbff243d848b43ca721d383493c23
ridwaanhall <erbyl14@gmail.com>
2024-12-12 14:03:34 +0700

    Add hero detail view URL to fetch hero information by ID

--- mlbb_web/urls.py
+++ mlbb_web/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.simple_view, name='simple_view'),
     path('hero-rank/', views.hero_rank_web, name='hero_rank_web'),
     path('hero-position/', views.hero_position_web, name='hero_position_web'),
+    path('hero-detail/<int:hero_id>/', views.hero_detail_web, name='hero_detail_web'),
 ]

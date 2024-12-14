68befc92ab024d7204c47241e55b34b6c7a13448
ridwaanhall <erbyl14@gmail.com>
2024-12-15 01:40:32 +0700

    Add hero list web view route to URL configuration

--- mlbb_web/urls.py
+++ mlbb_web/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.simple_view, name='simple_view'),
+    path('hero-list/', views.hero_list_web, name='hero_list_web'),
     path('hero-rank/', views.hero_rank_web, name='hero_rank_web'),
     path('hero-position/', views.hero_position_web, name='hero_position_web'),
     path('hero-detail/<int:hero_id>/', views.hero_detail_web, name='hero_detail_web'),

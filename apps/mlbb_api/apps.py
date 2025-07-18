394970a21aa41189d9c5e8fde7e01ce766cbe1c7
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-07-19 00:51:21 +0700

    feat(MPL): add mpl data Fixes #39

--- apps/mlbb_api/apps.py
+++ apps/mlbb_api/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MlbbApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mlbb_api'
+    name = 'apps.mlbb_api'
--- apps/mlbb_web/apps.py
+++ apps/mlbb_web/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MlbbWebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mlbb_web'
+    name = 'apps.mlbb_web'

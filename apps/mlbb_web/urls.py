b33f12470261e6cd4ce1809489be437ba5f02353
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-10-12 03:54:52 +0000

    feat(api): add endpoint to refresh hero cache

--- apps/mlbb_web/urls.py
+++ apps/mlbb_web/urls.py
@@ -13,4 +13,5 @@ if settings.IS_AVAILABLE:
         path('hero-rank/', views.MLBBWebViews.hero_rank_web, name='hero_rank_web'),
         path('hero-position/', views.MLBBWebViews.hero_position_web, name='hero_position_web'),
         path('hero-detail/<int:hero_id>/', views.MLBBWebViews.hero_detail_web, name='hero_detail_web'),
+        path('refresh-heroes/', views.MLBBWebViews.refresh_hero_cache_view, name='refresh_hero_cache'),
     ])

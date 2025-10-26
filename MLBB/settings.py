150042465e7978a0706d8a4d775b97859ca176bd
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-10-26 03:07:14 +0000

    fix(settings): change default IS_AVAILABLE value to True

--- MLBB/settings.py
+++ MLBB/settings.py
@@ -5,7 +5,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # API Availability Control
-IS_AVAILABLE = config('IS_AVAILABLE', default=False, cast=bool)
+IS_AVAILABLE = config('IS_AVAILABLE', default=True, cast=bool)
 
 SUPPORT_DETAILS = {
     'support_message': 'You can support us by donating from $1 USD (target: $500 USD) to help enhance API performance and handle high request volumes.',

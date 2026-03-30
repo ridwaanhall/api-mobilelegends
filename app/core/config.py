70c4c8f3e6a0a814711e4c8839a732efc7c91c7d
Ridwan Halim <hi@ridwaanhall.com>
2026-03-30 21:18:17 +0700

    🧹chore(pyproject): Setup project configuration

--- app/core/config.py
+++ app/core/config.py
@@ -19,7 +19,7 @@ DEBUG: bool = config("DEBUG", default=False, cast=bool)
 # =========================
 # Availability Settings
 # =========================
-API_VERSION: str = env_str("API_VERSION", default="3.2.0")
+API_VERSION: str = env_str("API_VERSION", default="3.1.2")
 IS_AVAILABLE: bool = config("IS_AVAILABLE", default=True, cast=bool)
 DATE_AVAILABLE: str = env_str("DATE_AVAILABLE", default="April 10, 2026")
 
